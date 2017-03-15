9113f58c8802540fdf47dda00a5731e88feba676
dariasteam <dariasteam94@gmail.com>
2017-03-15 21:09:58 +0000

    Añadida dependencia express

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-//var shell = require('gulp-shell');
-//var task = require('shell-task');
 var ghpages = require('gh-pages');
 var gitbook = require('gitbook');
 var connect = require("gulp-connect")
@@ -34,25 +32,25 @@ gulp.task('heroku', function(){
 //Ejecutar y luego desde el navegador, acceder a localhost:8000/user/id
 gulp.task('routing', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/Routing.js']));
+    .pipe(shell.exec('node ../src/Routing.js'));
 });
 gulp.task('BasicRouting', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/Br.js']));
+    .pipe(shell.exec(['node ../src/Br.js']));
 });
 gulp.task('middle1', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/middleware1.js']));
+    .pipe(shell.exec(['node ../src/middleware1.js']));
 });
 gulp.task('middle2', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/middleware2.js']));
+    .pipe(shell.exec(['node ../src/middleware2.js']));
 });
 gulp.task('middle3', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/middleware3.js']));
+    .pipe(shell.exec('node ../src/middleware3.js'));
 });
 gulp.task('middle4', function() {
   return gulp.src('')
-    .pipe(shell(['node ../src/middleware4.js']));
+    .pipe(shell.exec(['node ../src/middleware4.js']));
 });
